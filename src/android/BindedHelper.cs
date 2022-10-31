fa09f600a072e4a63af5a74da5867a3a2eae248e
Dan Ardelean <dan.ardelean@live.com>
2022-10-31 11:23:59 +0100

    Added helper using binded controls in .Net

---
+++ src/android/BindedHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.Content;
+using Android.Views;
+using Java.Lang;
+
+namespace lols
+{
+	public static class BindedHelper
+	{
+        public static TextView CreateTextView(Context context, Android.Graphics.Color color, float rotation, int x, int y)
+        {
+            TextView textView = new TextView(context);
+            textView.Text="lol?";
+            textView.SetTextColor(color);
+            textView.Rotation=rotation;
+
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+            layoutParams.LeftMargin = x;
+            layoutParams.TopMargin = y;
+            textView.LayoutParameters=layoutParams;
+
+            return textView;
+        }
+
+        public static void Add(RelativeLayout layout, TextView textView)
+        {
+            int childCount = layout.ChildCount;
+            if (childCount > 500)
+                layout.RemoveViewAt(childCount - 2);
+            layout.AddView(textView, 0);
+        }
+    }
+}
+
